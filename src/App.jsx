71bf0f7bf9600c9e390b01412b83f1b9556a23d4
Harshdev <harshdevsingh180@gmail.com>
2025-12-16 18:12:45 +0530

    add initial component and add categories

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-
-import './App.css'
+import { Routes, Route, Navigate } from "react-router-dom";
+import Categories from "./components/Categories";
 
 function App() {
-
   return (
-    <div className='text-4xl text-red-500 p-6 justify-center mx-auto flex'>
-      <h1>Welcome to the Furniture Website</h1>
-    </div>
-  )
+    <Routes>
+      <Route path="/categories" element={<Categories />} />
+      <Route path="*" element={<h1>404 Page Not Found</h1>} />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
